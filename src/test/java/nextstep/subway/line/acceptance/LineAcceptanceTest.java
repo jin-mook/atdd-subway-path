890bf82494240277d503994d02134ec76b3cad9b
Jin-Mook <jink0608@naver.com>
2024-07-23 16:27:02 +0900

    [step1] LineAcceptanceTest 에서 기본으로 역 2개를 생성하도록 sql 추가

--- src/test/java/nextstep/subway/line/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/acceptance/LineAcceptanceTest.java
@@ -9,7 +9,6 @@ import nextstep.subway.utils.LineAssuredTemplate;
 import nextstep.subway.utils.StationAssuredTemplate;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.groups.Tuple;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,26 +17,20 @@ import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
 
-@Sql(scripts = {"/delete-data.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@Sql(scripts = {"/delete-data.sql", "/create-default.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @DisplayName("지하철 노선 관리 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class LineAcceptanceTest {
 
-    private String upStation;
-    private String downStation;
+    private static final String UP_STATION = "상행종점역";
+    private static final String DOWN_STATION = "하행종점역";
 
-    private String lineName;
-    private String color;
-    private long distance;
+    private static final String LINE_NAME = "신분당선";
+    private static final String COLOR = "bg-red-600";
+    private static final long DISTANCE = 10;
 
-    @BeforeEach
-    void setUp() {
-        this.upStation = "상행종점역";
-        this.downStation = "하행종점역";
-        this.lineName = "신분당선";
-        this.color = "bg-red-600";
-        this.distance = 10;
-    }
+    private static final long UP_STATION_ID = 1;
+    private static final long DOWN_STATION_ID = 2;
 
     /**
      * Given 노선에 연결할 상행종점역, 하행종점역을 먼저 생성한다.
@@ -48,16 +41,9 @@ public class LineAcceptanceTest {
     @Test
     void createLine() {
         // given
-        long upStationId = StationAssuredTemplate.createStation(upStation)
-                .then()
-                .extract().jsonPath().getLong("id");
-
-        long downStationId = StationAssuredTemplate.createStation(downStation)
-                .then()
-                .extract().jsonPath().getLong("id");
 
         // when
-        LineRequest lineRequest = new LineRequest(lineName, color, upStationId, downStationId, distance);
+        LineRequest lineRequest = new LineRequest(LINE_NAME, COLOR, UP_STATION_ID, DOWN_STATION_ID, DISTANCE);
 
         ExtractableResponse<Response> result = LineAssuredTemplate.createLine(lineRequest)
                 .then()
@@ -65,11 +51,11 @@ public class LineAcceptanceTest {
 
         // then
         Assertions.assertThat(result.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-        Assertions.assertThat(result.body().jsonPath().getString("name")).isEqualTo(lineName);
-        Assertions.assertThat(result.body().jsonPath().getString("color")).isEqualTo(color);
+        Assertions.assertThat(result.body().jsonPath().getString("name")).isEqualTo(LINE_NAME);
+        Assertions.assertThat(result.body().jsonPath().getString("color")).isEqualTo(COLOR);
         Assertions.assertThat(result.body().jsonPath().getList("stations")).hasSize(2)
                 .extracting("name")
-                .contains(upStation, downStation);
+                .contains(UP_STATION, DOWN_STATION);
     }
 
     /**
@@ -82,21 +68,12 @@ public class LineAcceptanceTest {
     void showAllLines() {
         // given
         String newStation = "새로운지하쳘역";
-
-        long upStationId = StationAssuredTemplate.createStation(upStation)
-                .then()
-                .extract().jsonPath().getLong("id");
-
-        long downStationId = StationAssuredTemplate.createStation(downStation)
-                .then()
-                .extract().jsonPath().getLong("id");
-
         long newStationId = StationAssuredTemplate.createStation(newStation)
                 .then()
                 .extract().jsonPath().getLong("id");
 
-        LineAssuredTemplate.createLine(new LineRequest("신분당선", "bg-red-600", upStationId, downStationId, distance));
-        LineAssuredTemplate.createLine(new LineRequest("2호선", "bg-green-600", upStationId, newStationId, distance));
+        LineAssuredTemplate.createLine(new LineRequest("신분당선", "bg-red-600", UP_STATION_ID, DOWN_STATION_ID, DISTANCE));
+        LineAssuredTemplate.createLine(new LineRequest("2호선", "bg-green-600", UP_STATION_ID, newStationId, DISTANCE));
 
         // when
         ExtractableResponse<Response> result = LineAssuredTemplate.searchAllLine()
@@ -113,14 +90,14 @@ public class LineAcceptanceTest {
         Assertions.assertThat(responseData.get(0).getStations()).hasSize(2)
                 .extracting("id", "name")
                 .contains(
-                        Tuple.tuple(upStationId, upStation),
-                        Tuple.tuple(downStationId, downStation)
+                        Tuple.tuple(UP_STATION_ID, UP_STATION),
+                        Tuple.tuple(DOWN_STATION_ID, DOWN_STATION)
                 );
 
         Assertions.assertThat(responseData.get(1).getStations()).hasSize(2)
                 .extracting("id", "name")
                 .contains(
-                        Tuple.tuple(upStationId, upStation),
+                        Tuple.tuple(UP_STATION_ID, UP_STATION),
                         Tuple.tuple(newStationId, newStation)
                 );
     }
@@ -134,15 +111,7 @@ public class LineAcceptanceTest {
     @DisplayName("특정 지하철 노선을 조회합니다.")
     void findLine() {
         // given
-        long upStationId = StationAssuredTemplate.createStation(upStation)
-                .then()
-                .extract().jsonPath().getLong("id");
-
-        long downStationId = StationAssuredTemplate.createStation(downStation)
-                .then()
-                .extract().jsonPath().getLong("id");
-
-        LineRequest lineRequest = new LineRequest(lineName, color, upStationId, downStationId, distance);
+        LineRequest lineRequest = new LineRequest(LINE_NAME, COLOR, UP_STATION_ID, DOWN_STATION_ID, DISTANCE);
         long lineId = LineAssuredTemplate.createLine(lineRequest)
                 .then().extract().jsonPath().getLong("id");
 
@@ -153,11 +122,11 @@ public class LineAcceptanceTest {
 
         // then
         Assertions.assertThat(result.statusCode()).isEqualTo(HttpStatus.OK.value());
-        Assertions.assertThat(result.jsonPath().getString("name")).isEqualTo(lineName);
-        Assertions.assertThat(result.jsonPath().getString("color")).isEqualTo(color);
+        Assertions.assertThat(result.jsonPath().getString("name")).isEqualTo(LINE_NAME);
+        Assertions.assertThat(result.jsonPath().getString("color")).isEqualTo(COLOR);
         Assertions.assertThat(result.jsonPath().getList("stations")).hasSize(2)
                 .extracting("name")
-                .contains(upStation, downStation);
+                .contains(UP_STATION, DOWN_STATION);
     }
 
     /**
@@ -169,15 +138,7 @@ public class LineAcceptanceTest {
     @DisplayName("지하철 노선의 이름과 색 수정 요청을 하면 정상 응답을 받습니다. 이후 수정된 정보로 전달을 받습니다.")
     void updateLine() {
         // given
-        long upStationId = StationAssuredTemplate.createStation(upStation)
-                .then()
-                .extract().jsonPath().getLong("id");
-
-        long downStationId = StationAssuredTemplate.createStation(downStation)
-                .then()
-                .extract().jsonPath().getLong("id");
-
-        LineRequest lineRequest = new LineRequest(lineName, color, upStationId, downStationId, distance);
+        LineRequest lineRequest = new LineRequest(LINE_NAME, COLOR, UP_STATION_ID, DOWN_STATION_ID, DISTANCE);
         long lineId = LineAssuredTemplate.createLine(lineRequest)
                 .then().extract().jsonPath().getLong("id");
 
@@ -202,7 +163,7 @@ public class LineAcceptanceTest {
         Assertions.assertThat(findResult.jsonPath().getString("color")).isEqualTo(updateColor);
         Assertions.assertThat(findResult.jsonPath().getList("stations")).hasSize(2)
                 .extracting("name")
-                .contains(upStation, downStation);
+                .contains(UP_STATION, DOWN_STATION);
     }
 
     /**
@@ -214,15 +175,7 @@ public class LineAcceptanceTest {
     @DisplayName("지하철 노선 삭제 요청을 보내면 정상 응답을 전달받습니다. 이후 해당 노선은 보이지 않습니다.")
     void deleteLine() {
         // given
-        long upStationId = StationAssuredTemplate.createStation(upStation)
-                .then()
-                .extract().jsonPath().getLong("id");
-
-        long downStationId = StationAssuredTemplate.createStation(downStation)
-                .then()
-                .extract().jsonPath().getLong("id");
-
-        LineRequest lineRequest = new LineRequest(lineName, color, upStationId, downStationId, distance);
+        LineRequest lineRequest = new LineRequest(LINE_NAME, COLOR, UP_STATION_ID, DOWN_STATION_ID, DISTANCE);
         long lineId = LineAssuredTemplate.createLine(lineRequest)
                 .then().extract().jsonPath().getLong("id");
 
